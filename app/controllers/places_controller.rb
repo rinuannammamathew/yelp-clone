38015e2d64f7e0da22dfc9d30c823c818496997f
Rinu Mathew <rinuannammamathew@gmail.com>
2019-06-11 02:48:53 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-   before_action :authenticate_user!, only: [:new, :create]
+   before_action :authenticate_user!, only: [:new, :create, :edit]
 
 
   def index
@@ -19,7 +19,9 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
   def edit
-    @place = Place.find(params[:id])
+     if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
   end
   def update
     @place = Place.find(params[:id])
