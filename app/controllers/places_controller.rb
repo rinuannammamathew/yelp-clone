8d21721762114810f35412cae9246de261687eb6
Rinu Mathew <rinuannammamathew@gmail.com>
2019-06-17 05:43:30 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -26,26 +26,26 @@ class PlacesController < ApplicationController
     end
   end
   def update
-  @place = Place.find(params[:id])
+    @place = Place.find(params[:id])
 
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
-  end
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
 
-  @place.update_attributes(place_params)
-  if @place.valid?
-    redirect_to root_path
-  else
-    render :edit, status: :unprocessable_entity
+    @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
-end
 
   def destroy
      @place = Place.find(params[:id])
 
     if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+    return render plain: 'Not Allowed', status: :forbidden
+  end
 
      @place.destroy
      redirect_to root_path
